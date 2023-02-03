a5f8a9efafa91a6b8e025c508628b3b62383fe6a
Malathi S <malathi@realimage.com>
2023-02-04 01:17:55 +0530

    POC Phase1b implementation (#1)
    * FIFO implementation
    
    * Update README.md

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
-func saveLogs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: saveLogs")
+type ScheduleTag struct {
+	Id   int    `json:"id"`
+	Name string `json:"schedule"`
+}
+
+type PlaybackLog struct {
+	Id          int         `json:"id"`
+	ScheduleTag ScheduleTag `json:"scheduleTag"`
+	Screen      string      `json:"schedule"`
+	Cpl         string      `json:"orderId"`
+}
+
+var Queue []PlaybackLog
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "AgentQL homepage endpoint hit")
+}
+
+func receiveLogs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: receiveLogs")
 
 	responseData, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -16,21 +38,47 @@ func saveLogs(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Println(string(responseData))
-}
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "AgentQL homepage endpoint hit")
+	// pushes the received logs into queue
+	Ack := make([]PlaybackLog, 10)
+
+	json.Unmarshal(responseData, &Ack)
+
+	Queue = append(Queue, Ack...)
 }
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
 
-	// add our logs route and map it to our
-	// saveLogs function like so
-	http.HandleFunc("/saveLogs", saveLogs)
+	// add receiveLogs route and map it to our receiveLogs function like so
+	http.HandleFunc("/receiveLogs", receiveLogs)
 	log.Fatal(http.ListenAndServe(":8083", nil))
 }
 
 func main() {
-	handleRequests()
+	// initialise agentql apis
+	go handleRequests()
+
+	// end-less process
+	for {
+		rand.Seed(time.Now().UnixNano())
+		time.Sleep(time.Second * time.Duration(rand.Intn(10)))
+
+		// sends acknowledgement of log delivery
+		if len(Queue) != 0 {
+			jsonValue, _ := json.Marshal(Queue[0].Id)
+
+			_, err := http.Post("http://localhost:8082/ackFromAgentQL", "application/json", bytes.NewBuffer(jsonValue))
+
+			if err != nil {
+				fmt.Print(err.Error())
+			}
+		}
+
+		if len(Queue) > 1 {
+			Queue = Queue[1:]
+		} else {
+			Queue = make([]PlaybackLog, 0)
+		}
+	}
 }
